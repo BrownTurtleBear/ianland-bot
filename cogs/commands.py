7a51f22e817d52a3bf465e8a366b52af7be422a7
BrownTurtleBear <tommocaddy@gmail.com>
2024-07-01 18:27:22 +0930

    status command

--- cogs/commands.py
+++ cogs/commands.py
@@ -2,18 +2,37 @@ import discord
 from discord.ext import commands
 from discord import app_commands
 
+import time
+
 
 class Commands(commands.Cog):
     def __init__(self, client):
         self.client = client
+        self.colour = 0xf4d45e
+        global time_started
+        time_started = time.time()
 
     # github
-    @app_commands.command(name="github", description="The link to the bots repo")
-    async def github(self, interaction: discord.Interaction):
+    @app_commands.command(name="repo", description="The link to the bots repo")
+    async def repo(self, interaction: discord.Interaction):
         link = "https://github.com/BrownTurtleBear/ianland-bot"
-        github_embed = discord.Embed(title="Link to bots repo", url=link)
+        github_embed = discord.Embed(title="Link to Duckmin's repo", description="See how the bot works on the repo!\nAll suggestions are wellcome.", url=link, colour=self.colour)
         github_embed.set_thumbnail(url=self.client.user.avatar.url)
         await interaction.response.send_message(embed=github_embed)
+
+    @app_commands.command(name="status", description="How well is Duckmin running?")
+    async def status(self, interaction: discord.Interaction):
+        now = time.time()
+        uptime = now - time_started
+        uptime_sec = round(uptime)
+        uptime_min = round(uptime / 60)
+        uptime_hour = round(uptime / 3600)
+        uptime_day = round(uptime / 86400)
+        latency = round(self.client.latency * 1000)
+        status_embed = discord.Embed(title="Status", description=f"How well is Duckmin running?", colour=self.colour)
+        status_embed.add_field(name="📶 Ping 📶", value=f"Latency : {latency}ms.", inline=False)
+        status_embed.add_field(name="⌚ Uptime ⌚", value=f"{uptime_day}:{uptime_hour}:{uptime_min}:{uptime_sec}", inline=True)
+        await interaction.response.send_message(embed=status_embed)
 
 
 async def setup(client):
