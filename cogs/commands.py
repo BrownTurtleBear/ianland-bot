734b6f3fdee5dd1665f4a20524423a988b59ebbe
BrownTurtleBear <tommocaddy@gmail.com>
2024-07-01 17:57:56 +0930

    main

--- cogs/commands.py
+++ cogs/commands.py
@@ -9,11 +9,12 @@ class Commands(commands.Cog):
 
     # github
     @app_commands.command(name="github", description="The link to the bots repo")
-    async def github(self, interaction=discord.Interaction):
+    async def github(self, interaction: discord.Interaction):
         link = "https://github.com/BrownTurtleBear/ianland-bot"
         github_embed = discord.Embed(title="Link to bots repo", url=link)
         github_embed.set_thumbnail(url=self.client.user.avatar.url)
         await interaction.response.send_message(embed=github_embed)
+
 
 async def setup(client):
     await client.add_cog(Commands(client))
